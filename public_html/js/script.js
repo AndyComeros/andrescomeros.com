d8ec794dbba4405336cd9fe62d3196841f18fcd4
Andy <104561961+AndyComeros@users.noreply.github.com>
2023-01-19 18:28:19 +0800

    boid behaviour added

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -9,19 +9,24 @@ class SpaceShip{
         this.color = color;
         this.direction = new THREE.Vector3(randFloat(-1,1),randFloat(-1,1),randFloat(-1,1));
         this.direction.normalize();
-        this.speed = 0.1;
+        this.speed = 15;
 
-        this.lookRadius = 5;
-        this.lookLength = 2;
+        
+
+        this.seperateRad = 1.5;
+        this.cohesionRad = 5;
+        this.alignRad    = 4;
 
         //const myGeom = new THREE.ConeGeometry(1,2,10,10);
         const myGeom = new THREE.BoxGeometry();
         const myMat = new THREE.MeshBasicMaterial({color: this.color, wireframe: false,});
         this.myMesh = new THREE.Mesh(myGeom, myMat);
+       
 
         this.myMesh.position.x = 0;
         this.myMesh.position.y = 0;
         this.myMesh.position.z = 0;
+        this.myMesh.position.set(randFloat(-10,10),randFloat(-10,10),0);
 
         //steer vars
         let steerTotal;
@@ -33,50 +38,89 @@ class SpaceShip{
         
     }
 
-    seperate(ships){
 
-    }
-
+    //return vector pointing in average direction of neighbours
     align(ships){
+
+        let steer = new Vector3(0);
+        if(ships.length < 1)
+            return steer;
+
         for(let i = 0; i < ships.length; i++){
-            this.steerTotal.add(ships[i].direction);
+          steer.add(ships[i].direction);
         }
+        return steer.normalize();
     }
 
+    //return vector poiting towards average position of neighbours
     cohesion(ships){
+        let steer = new Vector3(0);
+        let avePos = new Vector3(0);
 
+        if(ships.length < 1)
+        return steer;
+
+        for(let i = 0; i < ships.length; i++){
+            avePos.add(ships[i].myMesh.position);
+        }
+
+        avePos.divideScalar(ships.length);
+
+        steer.subVectors(avePos,this.myMesh.position);
+
+        return steer;
+    }
+
+    //return vector pointing away from average position of neighbours
+    seperate(ships){
+    
+        return this.cohesion(ships).multiplyScalar(-1);
     }
 
 
 
     //do boid stuff
-    update(ships){
+    update(ships, deltaTime){
 
-        this.steerTotal = new Vector3(0);
-
-        let closeBoids = [];
+    
+        let aliBoid = [];
+        let sepBoid = [];
+        let cohBoid = [];
         for(let i = 0; i < ships.length; i++){
 
-            if(ships[i].id == this.id)
-                continue;
+            if(ships[i].id != this.id){
+                const dir = new Vector3();
+                dir.subVectors(ships[i].myMesh.position,this.myMesh.position);
                 
-
-            const dir = new Vector3();
-            dir.subVectors(ships[i].myMesh.position,this.myMesh.position);
-
-            if(Math.abs(dir.length()) < this.lookRadius){
-                
-                this.myMesh.material.color.set(0x00fff7);
-            }else{   
-                this.myMesh.material.color.set(0xff0000)
+                if(Math.abs(dir.length()) < this.cohesionRad){
+                    cohBoid.push(ships[i]);
+                }
+                if(Math.abs(dir.length()) < this.seperateRad){
+                    sepBoid.push(ships[i]);
+                }
+                if(Math.abs(dir.length()) < this.alignRad){
+                    aliBoid.push(ships[i]);
+                }
             }
-            
+
         }
 
-        this.seperate(closeBoids);
-        this.align(closeBoids);
-        this.cohesion(closeBoids);
+        //indicate boid in range or not
+        this.myMesh.material.color.set(0x00fff7);
+        if(aliBoid.length < 1){
+            this.myMesh.material.color.set(0xff0000);
+        }
 
+        
+
+        //add boid steer behavour
+        this.direction.add(this.seperate(sepBoid).multiplyScalar(0.05));
+        this.direction.add(this.   align(aliBoid).multiplyScalar(0.01));
+        this.direction.add(this.cohesion(cohBoid).multiplyScalar(0.01));
+        
+
+        this.direction.normalize();
+        
 
         //apply direction vector to position
         let look = new Vector3();
@@ -88,9 +132,9 @@ class SpaceShip{
         this.dirHelp.setLength(2);
        
 
-        this.myMesh.position.x += this.direction.x * this.speed;
-        this.myMesh.position.y += this.direction.y * this.speed;
-        this.myMesh.position.z += this.direction.z * this.speed;
+        this.myMesh.position.x += this.direction.x * this.speed * delta;
+        this.myMesh.position.y += this.direction.y * this.speed * delta;
+        this.myMesh.position.z += this.direction.z * this.speed * delta;
 
 
         //inside limited volume
@@ -113,9 +157,6 @@ class SpaceShip{
     
 
     }
-
-
-
 
 }
 
@@ -167,7 +208,7 @@ terrain.rotateX(80);
 scene.add(terrain);
 
 const ship = [];
-let shipCount = 100;
+let shipCount = 500;
 for(let i = 0; i < shipCount; i++){
     ship[i] = (new SpaceShip(i,0,0xff00fb));
     scene.add(ship[i].myMesh);
@@ -186,15 +227,22 @@ function onWindowResize(){
 }
 
 // main loop
+
+var clock = new THREE.Clock();
+var speed = 2;
+var delta = 0;
 function animate(){
 
-    terrainMat.uniforms.time.value += 0.01;
-    //cust.rotateY(0.1);
-    
+    //animate terrain
+    terrainMat.uniforms.time.value += 1 * delta;
+
+    //animate boids
     for(let i = 0; i < shipCount; i++){
         ship[i].update(ship)
     }
 
+
+    delta = clock.getDelta();
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
 
