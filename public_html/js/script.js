d3ea364ce9c10fcc458c3f0a16bd432b589c129b
Andy <104561961+AndyComeros@users.noreply.github.com>
2023-01-19 21:27:16 +0800

    small changes

--- public_html/js/script.js
+++ public_html/js/script.js
@@ -1,9 +1,9 @@
 import * as THREE from 'three';
 import { RedFormat, Vector3 } from 'three';
-import { randFloat } from 'three/src/math/MathUtils';
+import { randFloat, randInt } from 'three/src/math/MathUtils';
 
 class SpaceShip{
-    constructor(id,team, color){
+    constructor(id,team,color,mesh){
         this.id = id;
         this.team = team;
         this.color = color;
@@ -13,9 +13,15 @@ class SpaceShip{
 
         
 
-        this.seperateRad = 1.5;
-        this.cohesionRad = 5;
-        this.alignRad    = 4;
+        this.seperateRad = 3;
+        this.alignRad    = 6;
+        this.cohesionRad = 6;
+        
+
+        this.seperateStr = 0.02;
+        this.alignStr =      0.01;
+        this.cohesionStr = 0.01;
+        
 
         //const myGeom = new THREE.ConeGeometry(1,2,10,10);
         const myGeom = new THREE.BoxGeometry();
@@ -86,37 +92,31 @@ class SpaceShip{
         let aliBoid = [];
         let sepBoid = [];
         let cohBoid = [];
+
+        const inFront = new Vector3().addVectors(this.myMesh.position,this.direction.multiplyScalar(1.2));
         for(let i = 0; i < ships.length; i++){
 
             if(ships[i].id != this.id){
                 const dir = new Vector3();
                 dir.subVectors(ships[i].myMesh.position,this.myMesh.position);
                 
-                if(Math.abs(dir.length()) < this.cohesionRad){
+                if(Math.abs(dir.length()) < this.cohesionRad && this.team == ships[i].team){
                     cohBoid.push(ships[i]);
                 }
                 if(Math.abs(dir.length()) < this.seperateRad){
                     sepBoid.push(ships[i]);
                 }
-                if(Math.abs(dir.length()) < this.alignRad){
+                if(Math.abs(dir.length()) < this.alignRad && this.team == ships[i].team){
                     aliBoid.push(ships[i]);
                 }
             }
 
         }
-
-        //indicate boid in range or not
-        this.myMesh.material.color.set(0x00fff7);
-        if(aliBoid.length < 1){
-            this.myMesh.material.color.set(0xff0000);
-        }
-
-        
-
+    
         //add boid steer behavour
-        this.direction.add(this.seperate(sepBoid).multiplyScalar(0.05));
-        this.direction.add(this.   align(aliBoid).multiplyScalar(0.01));
-        this.direction.add(this.cohesion(cohBoid).multiplyScalar(0.01));
+        this.direction.add(this.seperate(sepBoid).multiplyScalar(this.seperateStr));
+        this.direction.add(this.   align(aliBoid).multiplyScalar(this.alignStr));
+        this.direction.add(this.cohesion(cohBoid).multiplyScalar(this.cohesionStr));
         
 
         this.direction.normalize();
@@ -137,9 +137,26 @@ class SpaceShip{
         this.myMesh.position.z += this.direction.z * this.speed * delta;
 
 
-        //inside limited volume
-        const boxSize = 50;
+        const boxSize = 100;
+        const centMagnitism = 0.2;
+        if(this.myMesh.position.x > boxSize/2)
+            this.direction.add(new Vector3(-1,0,0).multiplyScalar(centMagnitism ));
+        if(this.myMesh.position.x < boxSize/-2)
+        this.direction.add(new Vector3(1,0,0).multiplyScalar(centMagnitism ));
+            
+        if(this.myMesh.position.y > boxSize/2)
+            this.direction.add(new Vector3(0,-1,0).multiplyScalar(centMagnitism ));
+        if(this.myMesh.position.y < boxSize/-2)
+        this.direction.add(new Vector3(0,1,0).multiplyScalar(centMagnitism ));
+            
+        if(this.myMesh.position.z > boxSize/2)
+        this.direction.add(new Vector3(0,0,-1).multiplyScalar(centMagnitism ));
+        if(this.myMesh.position.z < boxSize/-2)
+        this.direction.add(new Vector3(0,0,1).multiplyScalar(centMagnitism ));
 
+        //inside limited volume
+        /*
+        const boxSize = 50;
         if(this.myMesh.position.x > boxSize/2)
             this.myMesh.position.x = boxSize/-2;
         if(this.myMesh.position.x < boxSize/-2)
@@ -154,7 +171,7 @@ class SpaceShip{
             this.myMesh.position.z = boxSize/-2;
         if(this.myMesh.position.z < boxSize/-2)
             this.myMesh.position.z = boxSize/2;
-    
+        */
 
     }
 
@@ -207,10 +224,13 @@ const terrain = new THREE.Mesh(geom, terrainMat);
 terrain.rotateX(80);
 scene.add(terrain);
 
+//spawn boids
 const ship = [];
-let shipCount = 500;
+let shipCount = 200;
+let colors = [0xff00fb,0x00fff7];
 for(let i = 0; i < shipCount; i++){
-    ship[i] = (new SpaceShip(i,0,0xff00fb));
+    let n = randInt(0,1);
+    ship[i] = (new SpaceShip(i,n, colors[n]));
     scene.add(ship[i].myMesh);
 }
 
